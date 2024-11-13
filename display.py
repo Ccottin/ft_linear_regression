3278582c807f18f1467448eca73d0724aa37bd89
Clara COTTIN <ccottin@paul-f4Ar8s5.clusters.42paris.fr>
2024-11-13 22:26:15 +0100

    greuh

--- display.py
+++ display.py
@@ -1,14 +1,39 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from pandas import DataFrame
 
 
-def display_dots(data, teta0, teta1):
-    data = data.set_index('km')
+def f(x, a, b):
+    return a * x + b
+
+def mse(data, to_print):
+    print(data)
+    to_print['error'] = data['estimated price'] * data['estimated price'] * 1 / len(data)
+
+def display_dots(data, variables):
+
+    to_print = DataFrame(columns=['x_values', 'y_values', 'error'])
+
+    to_print['x_values'] = np.linspace(-100, 100, 100)
+    to_print['y_values'] = f(to_print['x_values'], variables['teta1'], variables['teta0'])
+    mse(data, to_print)
+    
+
 
     plt.title('Car for sale')
     plt.ylabel('Price')
     plt.xlabel('Kilometers')
-    plt.plot(teta0, teta1, 'l')
-    plt.plot(data, 'o')
-    
+
+    plt.plot(data['km'], data['price'], 'o', label='points')
+    plt.plot(to_print['x_values'], to_print['y_values'], label="courbe")
+    plt.plot(data['estimated price'], to_print['error'], label="errors")
+    plt.legend()
+
+    plt.xlim(-10000, data['km'].max() + 10000)
+    plt.ylim(-1000, data['price'].max() + 1000)
+    plt.ylim()
+    plt.grid(True)
+    data.set_index('km')
+
     plt.show()
     plt.close()
--- main.py
+++ main.py
@@ -1,39 +1,45 @@
 from load_csv import load
 from display import display_dots
 from pandas import DataFrame
+import traceback
 import sys
 
-def estimate_price(mileage: int, teta0: int, teta1: int) -> int:
+def estimate_price(mileage: float, teta0: float, teta1: float) -> int:
     return (teta0 + (teta1 * mileage))
 
 
-def linear_regression(teta0: int, teta1: int, data: DataFrame):
+def linear_regression(variables, data: DataFrame):
     data['estimated price'] = None
     data['error'] = None
     data['coeff'] = None
     for i in range(len(data)):
-        data.at[i, 'estimated price'] = estimate_price(data.at[i, 'price'], teta0, teta1)
-        data.at[i, 'error'] = data.at[i, 'price'] - data.at[i, 'estimated price']
+        data.at[i, 'estimated price'] = estimate_price(data.at[i, 'price'],
+                        variables['teta0'], variables['teta1'])
+        data.at[i, 'error'] = data.at[i, 'estimated price'] - data.at[i, 'price']
         data.at[i, 'coeff'] = data.at[i, 'error'] * data.at[i, 'km']
-    tmpteta0 = data['error'].sum() * 1/len(data)
-    tmpteta1 = data['coeff'].sum() * 1/len(data)
+    variables['teta0'] -= data['error'].sum() * 1/len(data) * variables['learning_rate']
+    variables['teta1'] -= data['coeff'].sum() * 1/len(data) * variables['learning_rate']
     print(data)
-    print(tmpteta0, data['error'].mean())
-    print(tmpteta1)
-    display_dots(data, tmpteta0,tmpteta1)
+    print(variables['teta0'])
+    print(variables['teta1'])
+    display_dots(data, variables)
 
 
 def main():
     try:
-        assert len(sys.argv) == 4, "Please provide a single file and teta values"
+
+        assert len(sys.argv) == 5, "Please provide a single file and teta values"
         data = load(sys.argv[1])
         teta0 = float(sys.argv[2])
         teta1 = float(sys.argv[3])
-        print(data)
-        linear_regression(teta0, teta1, data)
+        learning_rate = float(sys.argv[4])
+        variables = {'teta0': teta0, 'teta1': teta1, 'learning_rate': learning_rate}
+        for i in range(3):
+            linear_regression(variables, data)
 
     except Exception as e:
         print("Error: ", str(e))
+        traceback.print_exc()
 
 
 if __name__ == "__main__":
