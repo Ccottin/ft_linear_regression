718fad4602c88a3ba168f018ac99d64bc58c59d2
Clara COTTIN <ccottin@paul-f4Br3s3.clusters.42paris.fr>
2024-11-15 16:48:23 +0100

    victoryyyyy! we finally manager to understand how and why std values are a necessity :) so we implemented std vals for everything. estimated price is then calculated with un-doing the standardisation process, and it give good vals

--- display.py
+++ display.py
@@ -6,27 +6,19 @@ from pandas import DataFrame
 def f(x, a, b):
     return a * x + b
 
-def mse(data, to_print):
-    print(data)
-    to_print['error'] = data['estimated price'] * data['estimated price'] * 1 / len(data)
-
-def display_dots(data, variables):
+def display_dots(data, vars):
 
     to_print = DataFrame(columns=['x_values', 'y_values', 'error'])
 
     to_print['x_values'] = np.linspace(0, data['km'].max(), 100)
-    to_print['y_values'] = f(to_print['x_values'], variables['teta1'], variables['teta0'])
-    #mse(data, to_print)
+    to_print['y_values'] = f(to_print['x_values'], vars['teta1'], vars['teta0'])
     
-
-
     plt.title('Car for sale')
     plt.ylabel('Price')
     plt.xlabel('Kilometers')
 
     plt.plot(data['km'], data['price'], 'o', label='points')
     plt.plot(to_print['x_values'], to_print['y_values'], label="courbe")
-    plt.plot(data['estimated price'], to_print['error'][0:24], label="errors")
     plt.legend()
 
     #plt.xlim(-10000, data['km'].max() + 10000)
@@ -36,7 +28,7 @@ def display_dots(data, variables):
     data.set_index('km')
 
     #print("to print = \n", to_print['error'][0:24])
-    print("to print = \n", to_print)
+   # print("to print = \n", to_print)
 
     plt.show()
     plt.close()
